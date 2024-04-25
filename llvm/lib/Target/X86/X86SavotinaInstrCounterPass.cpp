9b7dd0caa490580f9424aae38041edd371f324fa
Valeria <lerasavotina@gmail.com>
2024-04-25 21:53:50 +0300

    fix clang-format

--- llvm/lib/Target/X86/X86SavotinaInstrCounterPass.cpp
+++ llvm/lib/Target/X86/X86SavotinaInstrCounterPass.cpp
@@ -14,31 +14,35 @@ public:
     const TargetInstrInfo *tgtInstrInfo = nullptr;
     DebugLoc dbgLoc;
 
-    if (!machFunc.empty() && machFunc.front().begin() != machFunc.front().end()) {
+    if (!machFunc.empty() &&
+        machFunc.front().begin() != machFunc.front().end()) {
       tgtInstrInfo = machFunc.getSubtarget().getInstrInfo();
       dbgLoc = machFunc.front().begin()->getDebugLoc();
 
       if (tgtInstrInfo) {
         unsigned instrCount = 0;
-        unsigned regCount = machFunc.getRegInfo().createVirtualRegister(&X86::GR64RegClass);
-        
-        for (auto BBIter = machFunc.begin(); BBIter != machFunc.end(); ++BBIter) {
+        unsigned regCount =
+            machFunc.getRegInfo().createVirtualRegister(&X86::GR64RegClass);
+
+        for (auto BBIter = machFunc.begin(); BBIter != machFunc.end();
+             ++BBIter) {
           MachineBasicBlock &machBasicBlock = *BBIter;
-          
-          if (!machBasicBlock.empty()) { 
-            for (auto InstrIter = machBasicBlock.begin(); InstrIter != machBasicBlock.end(); ++InstrIter) {
+
+          if (!machBasicBlock.empty()) {
+            for (auto InstrIter = machBasicBlock.begin();
+                 InstrIter != machBasicBlock.end(); ++InstrIter) {
               instrCount++;
             }
           }
         }
 
-        BuildMI(machFunc.back(), machFunc.back().getFirstTerminator(),
-                    dbgLoc, tgtInstrInfo->get(X86::ADD64ri32), regCount)
-                .addReg(regCount)
-                .addImm(instrCount);
+        BuildMI(machFunc.back(), machFunc.back().getFirstTerminator(), dbgLoc,
+                tgtInstrInfo->get(X86::ADD64ri32), regCount)
+            .addReg(regCount)
+            .addImm(instrCount);
 
-        BuildMI(machFunc.back(), machFunc.back().getFirstTerminator(), 
-                dbgLoc, tgtInstrInfo->get(X86::MOV64mi32))
+        BuildMI(machFunc.back(), machFunc.back().getFirstTerminator(), dbgLoc,
+                tgtInstrInfo->get(X86::MOV64mi32))
             .addReg(regCount)
             .addImm(1)
             .addExternalSymbol("ic");
@@ -49,5 +53,7 @@ public:
   }
 };
 
-INITIALIZE_PASS(X86SavotinaInstrCounterPass, "x86-savotina-instr-counter",
-                "This pass let to instrCount the X86 machine instructions' number", false, false)
+INITIALIZE_PASS(
+    X86SavotinaInstrCounterPass, "x86-savotina-instr-counter",
+    "This pass let to instrCount the X86 machine instructions' number", false,
+    false)
